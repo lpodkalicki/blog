ab834f2b86b458d97601422a8940067d2d0dd44d
Łukasz Podkalicki <l.podkalicki@gmail.com>
2016-11-11 13:26:31 +0100

    Blinky with delay function for ATtiny2313.

---
+++ avr/attiny2313/001_blinky_with_delay_function/main.c
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2016, Łukasz Marcin Podkalicki <lpodkalicki@gmail.com>
+ * ATtiny2313/001
+ * Simple blinky with delay function.
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define	LED_PIN	PB0
+
+int
+main(void)
+{
+
+	/* setup */
+	DDRB = 0b00000001; // set LED pin as OUTPUT
+	PORTB = 0b00000000; // set all pins to LOW
+
+	/* loop */
+	while (1) {
+		PORTB ^= _BV(LED_PIN); // toggle LED pin
+		_delay_ms(500);
+	}
+}
