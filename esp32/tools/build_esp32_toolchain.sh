210503f567aec75a2b6310fe133623de37282b50
Łukasz Podkalicki <l.podkalicki@gmail.com>
2017-01-15 01:35:40 +0100

    Refactoring.

--- esp32/tools/build_esp32_toolchain.sh
+++ esp32/tools/build_esp32_toolchain.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Configuration
-WORKDIR=~
-TOOLCHAIN_BASEURL=https://dl.espressif.com/dl/
-TOOLCHAIN_FILEPATTERN=xtensa-esp32-elf-linux#arch#-1.22.0-61-gab8375a-5.2.0.tar.gz
+BASE_DIR=~/esp
+TOOLCHAIN_BASE_URL=https://dl.espressif.com/dl/
+TOOLCHAIN_FILE_PATTERN=xtensa-esp32-elf-linux#arch#-1.22.0-61-gab8375a-5.2.0.tar.gz
 
 
 # -------------------------------------------
@@ -13,7 +13,7 @@ do
 key="$1"
 case $key in
     -d|--directory)
-    WORKDIR="$2"
+    BASE_DIR="$2"
     shift
     ;;
     *)
@@ -26,13 +26,12 @@ done
 
 # -------------------------------------------
 # 1. Prepare base directory
-ESPDIR=$WORKDIR/esp
-if [ -d "$ESPDIR" ]; then
-	echo "Directory already exists: $ESPDIR, exiting..."
+if [ -d "$BASE_DIR" ]; then
+	echo "Base directory already exists: $BASE_DIR, exiting..."
 	exit 1
 fi
-mkdir -p $ESPDIR
-cd $ESPDIR
+mkdir -p $BASE_DIR
+cd $BASE_DIR
 
 
 # -------------------------------------------
@@ -43,13 +42,13 @@ sudo apt-get install git wget make libncurses-dev flex bison gperf python python
 # -------------------------------------------
 # 3. Download and unpack binary toolchain for ESP32
 if [ $(uname -m) == 'x86_64' ]; then
-	TOOLCHAIN_FILE=$(echo $TOOLCHAIN_FILEPATTERN | sed 's/#arch#/64/g')
+	TOOLCHAIN_FILE=$(echo $TOOLCHAIN_FILE_PATTERN | sed 's/#arch#/64/g')
 else
-	TOOLCHAIN_FILE=$(echo $TOOLCHAIN_FILEPATTERN | sed 's/#arch#/32/g')
+	TOOLCHAIN_FILE=$(echo $TOOLCHAIN_FILE_PATTERN | sed 's/#arch#/32/g')
 fi
-wget $TOOLCHAIN_BASEURL/$TOOLCHAIN_FILE
+wget $TOOLCHAIN_BASE_URL/$TOOLCHAIN_FILE
 tar -xzf $TOOLCHAIN_FILE
-TOOLCHAIN_DIR="$ESPDIR/xtensa-esp32-elf/bin"
+TOOLCHAIN_DIR="$BASE_DIR/xtensa-esp32-elf/bin"
 export PATH=$PATH:$TOOLCHAIN_DIR
 echo "export PATH=\$PATH:$TOOLCHAIN_DIR" >> ~/.bashrc
 
@@ -57,12 +56,13 @@ echo "export PATH=\$PATH:$TOOLCHAIN_DIR" >> ~/.bashrc
 # -------------------------------------------
 # 4. Get ESP-IDF from GitHub
 git clone --recursive https://github.com/espressif/esp-idf.git
-IDF_DIR="$ESPDIR/esp-idf"
+IDF_DIR="$BASE_DIR/esp-idf"
 export IDF_PATH=$IDF_DIR
 echo "export IDF_PATH=$IDF_DIR" >> ~/.bashrc
 
 
 echo "-------------------------------------------"
-echo "SUCCESS!"
+echo "Base directory: $BASE_DIR"
 echo "ESP-Toolchain directory: $TOOLCHAIN_DIR"
 echo "ESP-IDF directory: $IDF_DIR"
+echo "SUCCESS!"
