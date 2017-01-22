08e8796c32a1a17dd541f6bece00a202749f5fff
Łukasz Podkalicki <l.podkalicki@gmail.com>
2017-01-23 00:11:12 +0100

    Refactor.

--- esp32/016_wifi_sniffer/main/main.c
+++ esp32/016_wifi_sniffer/main/main.c
@@ -71,8 +71,9 @@ wifi_sniffer_init(void)
     	tcpip_adapter_init();
     	ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
     	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-	ESP_ERROR_CHECK( esp_wifi_set_country(WIFI_COUNTRY_EU) ); /* country Europe, channel range [1, 13] */    	
 	ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+	/* set country EU for channel range [1, 13] */
+	ESP_ERROR_CHECK( esp_wifi_set_country(WIFI_COUNTRY_EU) );
 	ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
     	ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_NULL) );
     	ESP_ERROR_CHECK( esp_wifi_start() );
@@ -110,13 +111,22 @@ wifi_sniffer_packet_handler(void* buff, wifi_promiscuous_pkt_type_t type)
 	const wifi_ieee80211_packet_t *ipkt = (wifi_ieee80211_packet_t *)ppkt->payload;
 	const wifi_ieee80211_mac_hdr_t *hdr = &ipkt->hdr;
 
-	printf("PACKET TYPE=%s, CHAN=%02d, RSSI=%02d, ADDR1=%02x:%02x:%02x:%02x:%02x:%02x, ADDR2=%02x:%02x:%02x:%02x:%02x:%02x, ADDR3=%02x:%02x:%02x:%02x:%02x:%02x\n", 
+	printf("PACKET TYPE=%s, CHAN=%02d, RSSI=%02d,"
+		" ADDR1=%02x:%02x:%02x:%02x:%02x:%02x,"
+		" ADDR2=%02x:%02x:%02x:%02x:%02x:%02x,"
+		" ADDR3=%02x:%02x:%02x:%02x:%02x:%02x\n",
 		wifi_sniffer_packet_type2str(type),
 		ppkt->rx_ctrl.channel,
 		ppkt->rx_ctrl.rssi,
-		hdr->addr1[0],hdr->addr1[1],hdr->addr1[2],hdr->addr1[3],hdr->addr1[4],hdr->addr1[5],
-		hdr->addr2[0],hdr->addr2[1],hdr->addr2[2],hdr->addr2[3],hdr->addr2[4],hdr->addr2[5],
-		hdr->addr3[0],hdr->addr3[1],hdr->addr3[2],hdr->addr3[3],hdr->addr3[4],hdr->addr3[5]
+		/* ADDR1 */
+		hdr->addr1[0],hdr->addr1[1],hdr->addr1[2],
+		hdr->addr1[3],hdr->addr1[4],hdr->addr1[5],
+		/* ADDR2 */
+		hdr->addr2[0],hdr->addr2[1],hdr->addr2[2],
+		hdr->addr2[3],hdr->addr2[4],hdr->addr2[5],
+		/* ADDR3 */
+		hdr->addr3[0],hdr->addr3[1],hdr->addr3[2],
+		hdr->addr3[3],hdr->addr3[4],hdr->addr3[5]
 	);
 }
 
