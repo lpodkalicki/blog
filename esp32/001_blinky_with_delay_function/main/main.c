7a3ba470f9d6c8eee1d6600e2150b37a0e7bd0e6
Łukasz Podkalicki <l.podkalicki@gmail.com>
2017-01-22 22:37:09 +0100

    Refactor.

--- esp32/001_blinky_with_delay_function/main/main.c
+++ esp32/001_blinky_with_delay_function/main/main.c
@@ -1,12 +1,14 @@
 /**
  * Copyright (c) 2017, Łukasz Marcin Podkalicki <lpodkalicki@gmail.com>
  * ESP32/001
- * Simple blinky with delay function.
+ * Blinky with delay function.
  */
 
 #include "freertos/FreeRTOS.h"
 #include "esp_event.h"
 #include "driver/gpio.h"
+
+#define	LED_GPIO_PIN	GPIO_NUM_4
 
 
 void
@@ -15,11 +17,11 @@ app_main(void)
 	int level = 0;
 
 	/* setup */
-	gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
+	gpio_set_direction(LED_GPIO_PIN, GPIO_MODE_OUTPUT);
 
 	/* loop */
 	while (true) {
-		gpio_set_level(GPIO_NUM_4, level = !level);
+		gpio_set_level(LED_GPIO_PIN, level ^= 1);
 		vTaskDelay(500 / portTICK_PERIOD_MS);
     	}
 }
