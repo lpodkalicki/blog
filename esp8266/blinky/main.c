72ca940ec2de2891338e31ce2294309438ccca3b
Łukasz Marcin Podkalicki <lpodkalicki@gmail.com>
2017-12-21 23:12:01 +0100

    Add first ESP8266 example - blinky with delay function.

---
+++ esp8266/blinky/main.c
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2017, Łukasz Marcin Podkalicki <lpodkalicki@gmail.com>
+ * ESP8266/001
+ * Blinky with delay function.
+ */
+
+#include <esp/gpio.h>
+#include "espressif/esp_common.h"
+#include "esp/uart.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+#define	LED_PIN		(2)
+
+static void
+blinky_task(void *prv)
+{
+
+	gpio_enable(LED_PIN, GPIO_OUTPUT);
+
+	while (1) {
+		gpio_write(LED_PIN, 1);
+                vTaskDelay(100 / portTICK_PERIOD_MS);
+		gpio_write(LED_PIN, 0);
+		vTaskDelay(100 / portTICK_PERIOD_MS);
+	}
+}
+
+void
+user_init(void)
+{
+
+	uart_set_baud(0, 115200);
+	xTaskCreate(blinky_task, "blinky_task", 256, NULL, 2, NULL);
+}
