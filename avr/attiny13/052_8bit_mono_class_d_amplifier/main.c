c949be32432b9b11d1fae646da88e50b86eaa585
Łukasz Podkalicki <lpodkalicki@gmail.com>
2019-02-04 12:20:54 +0100

    Add next ATtiny13 example - 8-bit mono Class D amplifier.

---
+++ avr/attiny13/052_8bit_mono_class_d_amplifier/main.c
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2019, Łukasz Marcin Podkalicki <lpodkalicki@gmail.com>
+ * ATtiny13/052
+ * Example of 8bit mono class D amplifier.
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#define	AUDIO_IN	PB2
+#define	AUDIO_OUT	PB0
+
+int
+main(void)
+{
+
+	/* setup */
+	DDRB |= _BV(AUDIO_OUT); // set AUDIO_OUT pin as output
+	TCCR0A |= _BV(WGM01)|_BV(WGM00); // set timer mode to FAST PWM
+        TCCR0A |= _BV(COM0A1); // connect PWM pin to channel A
+	TCCR0B |= _BV(CS00); // set prescaler to 1
+	ADMUX |= _BV(MUX0); // select ADC1 (PB2)
+	ADMUX |= _BV(REFS0); // set internal 1.1V reference voltage
+	ADMUX |= _BV(ADLAR); // left adjust of ADC result
+	ADCSRA |= _BV(ADPS1)|_BV(ADPS0); // set ADC division factor to 8
+        ADCSRA |= _BV(ADEN)|_BV(ADIE); // enable ADC and interrupt
+	ADCSRA |= _BV(ADATE); // set ADC auto-trigger
+	ADCSRA |= _BV(ADSC); // start conversion
+	sei(); // enable global interrupts
+
+	/* loop */
+	while (1);
+}
+
+ISR(ADC_vect)
+{
+
+	OCR0A = ADCH; // the magic
+}
