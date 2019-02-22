ad8c5f7076d1537ae95861b648c63d7cdb9d2bea
Łukasz Podkalicki <lpodkalicki@gmail.com>
2019-02-22 10:03:22 +0100

    Fix. Use ADC on PB3 instead of PB2 to reflect what is on the circuit.

--- avr/attiny13/052_8bit_mono_class_d_amplifier/main.c
+++ avr/attiny13/052_8bit_mono_class_d_amplifier/main.c
@@ -8,7 +8,7 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-#define	AUDIO_IN	PB2
+#define	AUDIO_IN	PB3
 #define	AUDIO_OUT	PB0
 
 int
@@ -18,13 +18,13 @@ main(void)
 	/* setup */
 	DDRB |= _BV(AUDIO_OUT); // set AUDIO_OUT pin as output
 	TCCR0A |= _BV(WGM01)|_BV(WGM00); // set timer mode to FAST PWM
-        TCCR0A |= _BV(COM0A1); // connect PWM pin to channel A
+	TCCR0A |= _BV(COM0A1); // connect PWM pin to channel A
 	TCCR0B |= _BV(CS00); // set prescaler to 1
-	ADMUX |= _BV(MUX0); // select ADC1 (PB2)
+	ADMUX |= _BV(MUX0)|_BV(MUX1); // select ADC3 (PB3)
 	ADMUX |= _BV(REFS0); // set internal 1.1V reference voltage
 	ADMUX |= _BV(ADLAR); // left adjust of ADC result
 	ADCSRA |= _BV(ADPS1)|_BV(ADPS0); // set ADC division factor to 8
-        ADCSRA |= _BV(ADEN)|_BV(ADIE); // enable ADC and interrupt
+	ADCSRA |= _BV(ADEN)|_BV(ADIE); // enable ADC and interrupt
 	ADCSRA |= _BV(ADATE); // set ADC auto-trigger
 	ADCSRA |= _BV(ADSC); // start conversion
 	sei(); // enable global interrupts
