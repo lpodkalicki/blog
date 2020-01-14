cf6c66a01e55f7ae2585ccd62822fa9a1bd86c9e
Łukasz Podkalicki <lpodkalicki@gmail.com>
2020-01-14 06:46:01 +0100

    Arduino - LED brightness.

---
+++ arduino/006_led_brightness/006_led_brightness.ino
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2020, Łukasz Marcin Podkalicki <lpodkalicki@gmail.com>
+ * Arduino/006
+ * Blinking LED with BasicLED class.
+ */
+
+#include <stdint.h>
+
+/**
+ * The instance of this class can control single LED.
+ * - on, off, toggle (any digital pin)
+ * - brightness (only PWM pins; i.e. for Arduino Uno: 3, 5, 6, 9, 10, 11)
+ */
+class BasicLed {
+public:
+  BasicLed(uint8_t pin): pin_(pin) {
+    pinMode(pin, OUTPUT); 
+  }
+  /**
+   * Light the LED up
+   */
+  void on(void) {
+    digitalWrite(pin_, HIGH);
+  }
+  /**
+   * Turn the LED off
+   */
+  void off(void) {
+    digitalWrite(pin_, LOW);
+  }
+  /**
+   * Toogle LED on/off
+   */
+  void toggle(void) {
+    digitalWrite(pin_, !digitalRead(pin_));
+  }
+  /**
+   * Set LED brightness from range <0, 255>
+   */
+  void setBrightness(uint8_t value) {
+    analogWrite(pin_, value);
+  }
+private:
+  uint8_t pin_;
+}; /* End of class BasicLed */
+
+BasicLed led(11);
+
+void setup() {
+  // do nothing
+}
+
+void loop() {
+    led.setBrightness(10); // 10/255 ~= 4%
+}
+
